c7003a3e0072e81378c51ac4fcfad30414d5f4da
KMR-repo <kmrchr051@gmail.com>
2019-07-25 00:03:26 +0530

    PL-SQL|Exception Handling

--- My_SQL/PL_SQL/Simple_PL-SQL2.sql
+++ My_SQL/PL_SQL/Simple_PL-SQL2.sql
@@ -25,6 +25,7 @@ BEGIN
     CLOSE kmrcur;
     dbms_output.put_line('******************END******************');
 END;
+/
 
 
 
@@ -57,3 +58,73 @@ FROM KMR_People;
     EXIT WHEN nCount > nRecCount;
     END LOOP;
 END;
+/
+
+/*PL-SQL with line ***** SIMPLE WAY OF USING CURSOR IN LOOP*/
+DECLARE
+CURSOR kmrcur IS
+    SELECT * 
+    FROM KMR_People;
+BEGIN
+    FOR kmr IN kmrcur LOOP
+        dbms_output.put_line ('City :' || kmr.city);
+    END LOOP;
+END;
+/
+
+/*PL-SQL Implicit Exceptions*/
+DECLARE
+vName KMR_People.Lastname%TYPE;
+vCity KMR_People.City%TYPE;
+BEGIN
+    SELECT lastname, city INTO vName, vCity
+    FROM KMR_People
+    WHERE person_id = 2;
+    dbms_output.put_line('NAME : ' || vName || 'City : '||vCity);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        dbms_output.put_line('No Data Found');
+    WHEN TOO_MANY_ROWS THEN
+        dbms_output.put_line('Too Many Records');
+    WHEN OTHERS THEN
+        dbms_output.put_line('Some Other Error');
+END;
+/
+
+/*String Building in PL-SQL*/
+DECLARE
+vString varchar2(255) := 'START : ';
+CURSOR strcur IS 
+    SELECT lastname
+    FROM KMR_People;
+BEGIN
+    FOR each IN strcur
+    LOOP
+    vString := vString || each.lastname || ' | ';
+    END LOOP;
+dbms_output.put_line('String : '||vString);
+END;
+/
+
+/*User defind exception PL-SQL*/
+DECLARE
+e_kmrException EXCEPTION;
+BEGIN
+    UPDATE KMR_People
+    SET city = 'City Updated'
+    WHERE person_id = 5;
+    
+    IF SQL%NOTFOUND THEN
+        RAISE e_kmrException;
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        dbms_output.put_line('No Records Found');
+    WHEN TOO_MANY_ROWS THEN
+        dbms_output.put_line('Too Many Records');
+    WHEN e_kmrException THEN
+        dbms_output.put_line('Selected record not found');
+    WHEN OTHERS THEN
+        dbms_output.put_line('Other Issues');
+END;
+/
